64179968afc39c2a7090053e7e5b263c58f4edca
Omayeli Arenyeka <yels7@yahoo.com>
2015-06-11 18:20:34 -0400

    input form
    input forms for twitter, gmail & sms

--- alarm/scripts/setup.js
+++ alarm/scripts/setup.js
@@ -1,5 +1,7 @@
 document.getElementById("userName").innerHTML = localStorage.getItem("user-name");
 console.log(localStorage.getItem("user-name"));
+
+
 var twitterDisplayed = true;
 var smsDisplayed = false;
 var gmailDisplayed = false;
@@ -7,6 +9,10 @@ var gmailDisplayed = false;
 $(function() {
 	$('#twitter').click(function () {
 		if (twitterDisplayed == false) {
+			$('#twitter-add').show();
+			$('#sms-add').hide();
+			$('#gmail-add').hide();
+			console.log("twitter");
 			twitterDisplayed = true;
 			gmailDisplayed = false;
 			smsDisplayed = false;
@@ -14,6 +20,10 @@ $(function() {
 	});
 	$('#gmail').click(function () {
 		if (gmailDisplayed == false) {
+			console.log("gmail");
+			$('#gmail-add').show();
+			$('#twitter-add').hide();
+			$('#sms-add').hide();
 			gmailDisplayed = true;
 			twitterDisplayed = false;
 			smsDisplayed = false;
@@ -22,6 +32,10 @@ $(function() {
 
 	$('#text').click(function () {
 		if (smsDisplayed == false) {
+			console.log("text");
+			$('#sms-add').show();
+			$('#twitter-add').hide();
+			$('#gmail-add').hide();
 			twitterDisplayed = false;
 			gmailDisplayed = false;
 			smsDisplayed = true;
