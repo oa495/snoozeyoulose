582a38b4d8d682add830ce8989baba78fce4b819
Omayeli Arenyeka <oa495@nyu.edu>
2015-06-20 12:41:35 -0400

    everything

--- alarm/scripts/setup.js
+++ alarm/scripts/setup.js
@@ -9,9 +9,46 @@ var number1;
 var twitterComplete = false;
 var smsComplete = false;
 var gmailComplete = false;
+var alarmStopped = false;
+var number1Valid, email1Valid, twitter1Valid;
+var alarmSet = false;
+
+window.onbeforeunload = function(e) {
+	if (alarmSet) {
+	 	return 'Are you sure you want to refresh? All your information will be lost.';
+	}
+};
 
 
 $(function() {
+	   var username;
+      
+      var username, UIState = {};
+      
+      UIState.authenticated = function() {
+        $('#login-form').addClass('hidden');
+        $('#logged-in').removeClass('hidden');
+        $('.username').text(username);
+      };
+      
+      UIState.unauthenticated = function() {
+        $('#login-form').removeClass('hidden');
+        $('#logged-in').addClass('hidden');
+        $('.username').text('');
+      };
+      
+      UIState.initial = function() {
+        console.log('initial');
+      
+        $audioRingIn[0].pause();
+        $audioRingOut[0].pause();
+
+      };    
+        $('#call-form p, #incoming-call p, #call-connected p').text('');
+        $('#incoming-call, #call-connected, .call-terminator, #resume-call-btn').addClass('hidden');
+        $('#call-form, .call-initializer').removeClass('hidden')
+
+    var userArray = [];
 	$('#twitter').click(function () {
 		if (twitterDisplayed == false) {
 			$('#twitter-add').show();
@@ -46,31 +83,121 @@ $(function() {
 			smsDisplayed = true;
 		}
 	});
-	$('#twitter-submit').click(function() {
-		tHandle1 = $("#twitterHandle1").val();
-		console.log(tHandle1);
-		localStorage.setItem("twitter1", tHandle1);
-		$("#twitter-submit").html("Thanks!");
-		event.preventDefault();
-		twitterComplete = true;
+
+
+	var maxCharacters = 130;
+	var characters = $('#tweet').val().length;
+	var twitterHandle = $('#twitterHandle1').val().length;
+	var total = characters + twitterHandle;
+	var overCharacters = false;
+	$('#count').text(maxCharacters - total);
+
+	$('#tweet, #twitterHandle1').bind('keyup keydown', function() {
+		var count = $('#count');
+		characters = $('#tweet').val().length;
+		twitterHandle = $('#twitterHandle1').val().length;
+		total = characters + twitterHandle;
+		if (total > maxCharacters) {
+		    count.addClass('over');
+		    overCharacters = true;
+		} else {
+		    count.removeClass('over');
+		}
+		count.text(maxCharacters - total);
 	});
+
+
+	$('#twitter-submit').click(function() {
+		event.preventDefault();
+		tHandle1 = $("#twitterHandle1").val();
+		if (tHandle1 == null || tHandle1 == '' || tHandle1 == "@twitterhandle1") {
+			console.log("twitter wrong");
+			twitter1Valid = false;
+			$('#twitterHandle1').attr('class', 'incomplete');
+		}
+		else if (tHandle1.indexOf('@') != 0) {
+			console.log("twitter wrong 1");
+			twitter1Valid = false;
+		    $('#twitterHandle1').attr('class', 'incomplete');
+		    tHandle1.textContent = "Remember to put the @ sign.";
+		}
+		else {
+			if (!overCharacters) {
+				console.log("twitter true");
+				 $('#twitterHandle1').attr('class', 'complete');
+				localStorage.setItem("twitter1", tHandle1);
+				$("#twitter-submit").html("Thanks!");
+				twitterComplete = true;
+			}
+		}
+	});
+
 	$('#gmail-submit').click(function() {
 		event.preventDefault();
 		email1 = $("#emailTo1").val();
-		console.log(email1);
-		localStorage.setItem("email1", email1);
-		$("#gmail-submit").html("Thanks!");
-		event.preventDefault();
-		gmailComplete = true;
+		//console.log(email1);
+		 if (email1 == null || email1 == '' || email1 == "email@email.com") {
+		 	//console.log("gmail wrong1");
+		    email1Valid = false;
+		     $('#emailTo1').attr('class', 'incomplete');
+		  } else if (email1.indexOf('@') == -1) { // if no @ symbol
+		  	//console.log("gmail wrong");
+		    email1Valid = false;
+		     $('#emailTo1').attr('class', 'incomplete');
+		    email1.textContent = "Please enter a valid email address.";
+		  } else {
+		  //	console.log("gmail true");
+		    emailValid = true;
+		    $('#emailTo1').attr('class', 'complete');
+		    localStorage.setItem("email1", email1);
+			$("#gmail-submit").html("Thanks!");
+			gmailComplete = true;
+		  }
 	});
+
 	$('#sms-submit').click(function() {
-		number1 = $("#number1").val();
-		console.log(number1);
-		localStorage.setItem("number1", number1);
-		$("#sms-submit").html("Thanks!");
 		event.preventDefault();
-		smsComplete = true;
+		username = 'user1@snoozeyoulose.gmail.com'
+        var apiKey = 'DAK02c0a220842d4a4ea87824de0474cd0f'
+        var password = '1voluptasaliquame1'
+      
+        /** login(domainApiId, userName, password,success,failure)
+            logs in user to Kandy Platform
+            @params <string> domainApiId, <string> userName, <string> password, <function> success/failure
+        */
+        kandy.login(apiKey, username, password,function(msg){
+      
+               userArray.push(username);
+               kandy.getLastSeen(userArray);
+               UIState.authenticated();
+           },	
+           function(msg){
+               UIState.unauthenticated();
+               alert('Login Failed!');
+           });
+        number1 = $("#number1").val();
+
+        if (number1 == null || number1 == '' || number1 == "19100000000") {
+        	console.log("wrong 1");
+        	number1Valid = false;
+        	$("#number1").attr('class', 'incomplete');
+        }
+        else if (number1.substring(0, 1) != 1 || number1.length !== 11) {
+        	console.log("wrong 2");
+        	number1Valid = false;
+        	number1.textContent = "Make sure you add 1 to the beginning";
+        	$("#number1").attr('class', 'incomplete');
+        }
+        else {
+        	console.log("right");
+        	localStorage.setItem("number1", number1);
+        	$("#sms-submit").html("Thanks!");
+        	$("#number1").attr('class', 'complete');
+			smsComplete = true;
+        }
+		console.log(number1);
 	});
+
 	function complete() {
 		if ((smsComplete) && (gmailComplete) && (twitterComplete)) {
 			console.log("everything!");
@@ -83,7 +210,6 @@ $(function() {
 		}
 	}
 
-
 	var ifDone = setInterval(complete, 1000); 
 });
 
@@ -92,6 +218,7 @@ $(function() {
 	var clock = $('#clock');
 	var alarm = clock.find('.alarm');
 	var ampm = clock.find('.ampm');
+	var timeIsUp = $('#time-is-up').parent();
 
 	var digitToName = 'zero, one, two, three, four, five, six, seven, eight, nine'.split(',');
 	var digits = {};
@@ -190,6 +317,7 @@ $(function() {
 	});
 
 	$('#setTime').click(function () {
+		alarmSet = true;
    		indicatedTime = $('#time').combodate('getValue');
    		$('.setup').fadeOut( "slow", function() {
    			if ($('.connect').css('display') == 'none') {
@@ -203,11 +331,48 @@ $(function() {
 		if (timeNow === indicatedTime) {
 			console.log("yay!");
 			clearInterval(alarmTime);
+			timeIsUp.fadeIn();
       	 	$('#alarm-ring')[0].play();
+      	 	setTimeout(sendMessages, 2000);
+      	 	//change to 60s 
       	 	console.log("played!");
 		}
 	};
 
+	function sendMessages() {
+		$("#end-alarm").click(function(){
+			console.log("ended");
+			timeIsUp.fadeOut();
+			alarmStopped = true;
+		});
+
+		if (alarmStopped != true) {
+			setTimeout(function() {
+			console.log("you're screwed");
+			  var sender = localStorage.getItem("user-name");
+			  console.log(number1);
+			  console.log(sender);
+	          var receiver = number1;
+	          var message = "hey sup";
+	      
+
+	      	//kandy.messaging.sendSMS(number (string), sender(object), text (string), success, failure) : Void
+
+	      	//FOR RECEIVER, PUT COUNTRY CODE FIRST, NO NON-NUMERIC CHARACTERS
+	          kandy.messaging.sendSMS(
+	            receiver,
+	            sender,
+	            message,
+	            function() {
+	              alert('sms sent');
+	            },
+	            function(message, status) {
+	              alert(message + status + ' message not sent!');
+	            }
+	          );
+			}, 10000);
+		}
+	}
 	var alarmTime = setInterval(check_time, 1000); 
 
 });
